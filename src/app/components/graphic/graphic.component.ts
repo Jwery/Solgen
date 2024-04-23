eb3000adea4ad228b9ca20e70d11f04f465f523a
JoatypeR <weryjoachim@gmail.com>
2024-04-23 16:23:04 +0200

    Plus d'erreur, recommence implémentation API

--- src/app/components/graphic/graphic.component.ts
+++ src/app/components/graphic/graphic.component.ts
@@ -6,73 +6,82 @@ import { Component, OnInit } from '@angular/core';
     templateUrl: './graphic.component.html'
 })
 export class GraphicComponent implements OnInit {
-  data: any;
+    data: any;
 
-  options: any;
+    options: any;
 
-  ngOnInit() {
-      const documentStyle = getComputedStyle(document.documentElement);
-      const textColor = documentStyle.getPropertyValue('--text-color');
-      const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
-      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
-      
-      this.data = {
-          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-          datasets: [
-              {
-                  label: 'First Dataset',
-                  data: [65, 59, 80, 81, 56, 55, 40],
-                  fill: false,
-                  tension: 0.4,
-                  borderColor: documentStyle.getPropertyValue('--blue-500')
-              },
-              {
-                  label: 'Second Dataset',
-                  data: [28, 48, 40, 19, 86, 27, 90],
-                  fill: false,
-                  borderDash: [5, 5],
-                  tension: 0.4,
-                  borderColor: documentStyle.getPropertyValue('--teal-500')
-              },
-              {
-                  label: 'Third Dataset',
-                  data: [12, 51, 62, 33, 21, 62, 45],
-                  fill: true,
-                  borderColor: documentStyle.getPropertyValue('--orange-500'),
-                  tension: 0.4,
-                  backgroundColor: 'rgba(255,167,38,0.2)'
-              }
-          ]
-      };
-      
-      this.options = {
-          maintainAspectRatio: false,
-          aspectRatio: 0.6,
-          plugins: {
-              legend: {
-                  labels: {
-                      color: textColor
-                  }
-              }
-          },
-          scales: {
-              x: {
-                  ticks: {
-                      color: textColorSecondary
-                  },
-                  grid: {
-                      color: surfaceBorder
-                  }
-              },
-              y: {
-                  ticks: {
-                      color: textColorSecondary
-                  },
-                  grid: {
-                      color: surfaceBorder
-                  }
-              }
-          }
-      };
-  }
+    ngOnInit() {
+        const documentStyle = getComputedStyle(document.documentElement);
+        const textColor = documentStyle.getPropertyValue('--text-color');
+        const textColorSecondary = documentStyle.getPropertyValue('--text-color-secondary');
+        const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+        
+        this.data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+            datasets: [
+                {
+                    label: 'Dataset 1',
+                    fill: false,
+                    borderColor: documentStyle.getPropertyValue('--blue-500'),
+                    yAxisID: 'y',
+                    tension: 0.4,
+                    data: [65, 59, 80, 81, 56, 55, 10]
+                },
+                {
+                    label: 'Dataset 2',
+                    fill: false,
+                    borderColor: documentStyle.getPropertyValue('--green-500'),
+                    yAxisID: 'y1',
+                    tension: 0.4,
+                    data: [28, 48, 40, 19, 86, 27, 90]
+                }
+            ]
+        };
+        
+        this.options = {
+            stacked: false,
+            maintainAspectRatio: false,
+            aspectRatio: 0.6,
+            plugins: {
+                legend: {
+                    labels: {
+                        color: textColor
+                    }
+                }
+            },
+            scales: {
+                x: {
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder
+                    }
+                },
+                y: {
+                    type: 'linear',
+                    display: true,
+                    position: 'left',
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        color: surfaceBorder
+                    }
+                },
+                y1: {
+                    type: 'linear',
+                    display: true,
+                    position: 'right',
+                    ticks: {
+                        color: textColorSecondary
+                    },
+                    grid: {
+                        drawOnChartArea: false,
+                        color: surfaceBorder
+                    }
+                }
+            }
+        };
+    }
 }
--- src/app/components/toggles/toggles.component.ts
+++ src/app/components/toggles/toggles.component.ts
@@ -1,16 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-toggles',
   templateUrl: './toggles.component.html',
   styleUrl: './toggles.component.scss'
 })
-export class TogglesComponent  {
-  VillesList: any;
-  service: any;
-  ngOnInit(): void {
-    this.service.getDepList().subscribe((data:any)=>{
-  this.VillesList=data;
-});
-}
+
+export class TogglesComponent   {
+  
 }
