989f18f62075fa465107862fda518bfe00b60179
JoatypeR <weryjoachim@gmail.com>
2024-05-13 12:09:48 +0200

    Service infochantier commit before merge

--- src/app/components/worksite/worksite.component.ts
+++ src/app/components/worksite/worksite.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { CityService } from '../../services/city.service'
+import { CityService } from '../../services/city.service';
+import { InfochantierService } from '../../services/infochantier.service'; // Importez le service InfochantierService
 
 @Component({
   selector: 'app-worksite',
@@ -13,7 +14,7 @@ export class WorksiteComponent implements OnInit {
     CityList:string[] = [];
     SelectedCity:string|null = null;
   
-    constructor(private http: HttpClient, private cityService: CityService) {
+    constructor(private http: HttpClient, private cityService: CityService, private infoChantierService: InfochantierService) {
     }
   
     ngOnInit() {
@@ -27,8 +28,21 @@ export class WorksiteComponent implements OnInit {
             this.CityList=citys
         }),
         error:(err => console.error(err))
-    
-   });
+     });
+   }
 
+   // Méthode appelée lors de la sélection de la ville
+   onCityChange(city: string) {
+     this.infoChantierService.addCityToSite(city)
+       .subscribe({
+         next: () => {
+           console.log('Ville ajoutée avec succès à la base de données.');
+           // Ajoutez ici toute autre logique nécessaire après l'ajout de la ville
+         },
+         error: (err) => {
+           console.error('Erreur lors de l\'ajout de la ville : ', err);
+           // Ajoutez ici toute autre logique de gestion d'erreur
+         }
+       });
    }
 }
--- src/app/services/infochantier.service.ts
+++ src/app/services/infochantier.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class InfochantierService {
 
-  constructor() { }
+  constructor(private http: HttpClient, private cookieservice: CookieService) { }
+
+  // Méthode pour envoyer la ville à l'API
+  addCityToSite(site: string) {
+    const headers = { 'Authorization': 'Bearer  '+ this.cookieservice.get('token') }
+    return this.http.post('/api/Sites', { headers });
+  }
 }
