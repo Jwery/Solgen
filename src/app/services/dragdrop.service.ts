b8504ee45f662cee02fc47021d6b505640e2ac97
Jwery <weryjoachim@gmail.com>
2024-05-17 17:57:34 +0200

    feat: upgrade drag and drop functionality and initialize API communication service
    - Upgraded the drag and drop functionality to enhance user experience.
    - Initialized the service to facilitate communication with the API.

--- src/app/services/dragdrop.service.ts
+++ src/app/services/dragdrop.service.ts
@@ -8,13 +8,18 @@ import { CookieService } from 'ngx-cookie-service';
 })
 export class DragdropService {
 
-  private apiUrl = '/api/Essay/FromFile';
+  private apiUrl = '/api/Essay/FromFile/';
 
   constructor(private http: HttpClient, private cookieservice: CookieService) { }
 
+  uploadFile(fileData: FormData): Observable<any> {
+    const headers = { 'Authorization': 'Bearer ' + this.cookieservice.get('token') };
+    return this.http.post(`${this.apiUrl}/UploadFile`, fileData, { headers });
+  }
+
   GetEssayFromFile(fileName: string): Observable<any> {
-  const headers = { 'Authorization': 'Bearer  '+ this.cookieservice.get('token') }
-    const url = `${this.apiUrl}/${fileName}`;
-    return this.http.get(url, {headers});
+    const headers = { 'Authorization': 'Bearer ' + this.cookieservice.get('token') };
+    const url = `${this.apiUrl}/FromFile/${fileName}`;
+    return this.http.get(url, { headers });
   }
 }
