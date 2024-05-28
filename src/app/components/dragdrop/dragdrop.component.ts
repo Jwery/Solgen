49d6d1c3feac73ece01fe309c5462a4dcf735b3e
JoatypeR <weryjoachim@gmail.com>
2024-05-28 13:45:31 +0200

    Trying to fic the drag and drop component, but it give a Error 404

--- src/app/components/dragdrop/dragdrop.component.ts
+++ src/app/components/dragdrop/dragdrop.component.ts
@@ -1,13 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { MessageService } from 'primeng/api';
 import { DragdropService } from '../../services/dragdrop.service';
-import { HttpClient } from '@angular/common/http';
 import { FormGroup, FormBuilder } from '@angular/forms';
-
-interface UploadEvent {
-  originalEvent: Event;
-  files: File[];
-}
 
 @Component({
   selector: 'DragdropComponent',
@@ -19,36 +13,38 @@ export class DragdropComponent {
   gruData: any;
   uploadedFiles: any[] = [];
   productForm: FormGroup;
-  @Input() essayId:number=0
+  @Input() essayId: number = 0;
 
   constructor(private messageService: MessageService, private dragdropService: DragdropService, private formBuilder: FormBuilder) {
     this.productForm = this.formBuilder.group({
       File: [null],
-  })
+    });
   }
 
   onUpload(event: any) {
-    if (event.files && event.files.length > 0) {
-      for (let file of event.files) {
+    const files = event.files || event.originalEvent?.dataTransfer?.files || event.target?.files;
+
+    if (files && files.length > 0) {
+      for (let file of files) {
         // Vérification de l'extension du fichier
         if (!this.isValidFileExtension(file)) {
-          this.messageService.add({severity: 'error', summary: 'Error', detail: 'Seuls les fichiers avec l\'extension .GRU sont autorisés.'});
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Seuls les fichiers avec l\'extension .GRU sont autorisés.' });
           return;
         }
 
         const formData = new FormData();
         formData.append('file', file);
-        this.productForm.controls['File'].setValue(event.target.files[0]);
+        this.productForm.controls['File'].setValue(file);
 
-        this.dragdropService.uploadFile(this.productForm,this.essayId).subscribe(
+        this.dragdropService.uploadFile(this.productForm, this.essayId).subscribe(
           (data) => {
             this.gruData = data;
             this.uploadedFiles.push(file);
-            this.messageService.add({severity: 'info', summary: 'File Uploaded', detail: file.name + ' uploadé avec succès.'});
+            this.messageService.add({ severity: 'info', summary: 'File Uploaded', detail: file.name + ' uploadé avec succès.' });
           },
           (error) => {
             console.error('Erreur lors de l\'upload du fichier :', error);
-            this.messageService.add({severity: 'error', summary: 'Error', detail: 'Erreur lors de l\'upload du fichier.'});
+            this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Erreur lors de l\'upload du fichier.' });
           }
         );
       }
--- src/app/services/dragdrop.service.ts
+++ src/app/services/dragdrop.service.ts
@@ -2,20 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
-import {FormGroup} from '@angular/forms'
+import { FormGroup } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DragdropService {
 
-  private apiUrl = '/api/Essay/FromFile';
+  private apiUrl = 'http://localhost:3000/api/Essay/FromFile';  // Assurez-vous que l'URL est correcte
 
   constructor(private http: HttpClient, private cookieservice: CookieService) { }
 
-  uploadFile(fileData: FormGroup,idValue:number): Observable<any> {
+  uploadFile(fileData: FormGroup, idValue: number): Observable<any> {
     const headers = { 'Authorization': 'Bearer ' + this.cookieservice.get('token') };
-    return this.http.post(`${this.apiUrl}/UploadFile/`+idValue, fileData.value, { headers });
+    return this.http.post(`${this.apiUrl}/UploadFile/${idValue}`, fileData.value, { headers });
   }
 
   GetEssayFromFile(fileName: string): Observable<any> {
